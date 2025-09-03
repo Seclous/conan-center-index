a09d897715f10db2c02d20a99f64b89972954b90
Hannah Sauermann <hannah.sauermann@seclous.com>
2025-09-03 19:00:22 +0200

    WIP

--- recipes/xsd/all/conanfile.py
+++ recipes/xsd/all/conanfile.py
@@ -3,7 +3,10 @@ import os
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
-from conan.tools.files import apply_conandata_patches, chdir, copy, export_conandata_patches, get, rmdir
+from conan.tools.files import (
+    apply_conandata_patches, chdir, copy, export_conandata_patches,
+    get, rmdir
+)
 from conan.tools.gnu import Autotools, AutotoolsDeps, AutotoolsToolchain
 from conan.tools.layout import basic_layout
 
@@ -20,10 +23,14 @@ class ConanXqilla(ConanFile):
     license = ("GPL-2.0", "FLOSSE")
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://codesynthesis.com/projects/xsd/"
-    topics = ("xml", "c++")
+    topics = ("xml", "xsd", "codegen", "c++")
 
     package_type = "application"
     settings = "os", "arch", "compiler", "build_type"
+
+    def build_requirements(self):
+        if str(self.settings.os) == "Windows":
+            self.tool_requires("msys2/cci.latest")
 
     def export_sources(self):
         export_conandata_patches(self)
@@ -32,14 +39,15 @@ class ConanXqilla(ConanFile):
         basic_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("xerces-c/3.2.3")
+        self.requires("xerces-c/[>=3.2.3 <4]")
 
     def package_id(self):
         del self.info.settings.compiler
 
     def validate(self):
-        if self.settings.os not in ["Linux", "FreeBSD"]:
-            raise ConanInvalidConfiguration("The xsd recipe currently only supports Linux.")
+        os_ = str(self.settings.os)
+        if os_ not in ["Linux", "FreeBSD", "Macos", "Windows"]:
+            raise ConanInvalidConfiguration(f"Unsupported OS: {os_}")
         if self.settings.compiler.cppstd:
             check_min_cppstd(self, 11)
 
@@ -48,11 +56,15 @@ class ConanXqilla(ConanFile):
 
     def generate(self):
         tc = AutotoolsToolchain(self)
+        # Upstream makefiles respect CXXFLAGS/LDFLAGS
         tc.extra_cxxflags = ["-std=c++11"]
-        tc.extra_ldflags = ["-pthread"]
+        # pthread needed on POSIX; MinGW’s pthreads is provided by the toolchain
+        if str(self.settings.os) in ["Linux", "FreeBSD", "Macos"]:
+            tc.extra_ldflags = ["-pthread"]
         tc.generate()
-        tc = AutotoolsDeps(self)
-        tc.generate()
+
+        deps = AutotoolsDeps(self)
+        deps.generate()
 
     def build(self):
         apply_conandata_patches(self)
@@ -70,6 +82,7 @@ class ConanXqilla(ConanFile):
         copy(self, "FLOSSE",
              dst=os.path.join(self.package_folder, "licenses"),
              src=os.path.join(self.source_folder, "xsd"))
+
         with chdir(self, self.source_folder):
             autotools = Autotools(self)
             autotools.install(args=[f"install_prefix={self.package_folder}"])
@@ -80,8 +93,3 @@ class ConanXqilla(ConanFile):
         self.cpp_info.libdirs = []
         self.cpp_info.resdirs = []
         self.cpp_info.includedirs = []
-
-        # TODO: to remove in conan v2
-        bin_path = os.path.join(self.package_folder, "bin")
-        self.output.info(f"Appending PATH environment variable: {bin_path}")
-        self.env_info.path.append(bin_path)
