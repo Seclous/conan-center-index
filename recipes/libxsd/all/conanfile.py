b68e157ac060415cbba6a2dcd9e461397aa993a1
Hannah Sauermann <hannah.sauermann@seclous.com>
2026-05-14 13:11:13 +0200

    Bugfix for incorrectly formatted datetimes for some locales

--- recipes/libxsd/all/conanfile.py
+++ recipes/libxsd/all/conanfile.py
@@ -1,5 +1,5 @@
 from conan import ConanFile
-from conan.tools.files import copy, get, rm
+from conan.tools.files import apply_conandata_patches, copy, get, rm
 from conan.tools.layout import basic_layout
 import os
 
@@ -17,6 +17,7 @@ class LibXsdConan(ConanFile):
     package_type = "header-library"
     settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
+    exports_sources = "patches/*"
 
     def layout(self):
         basic_layout(self, src_folder="src")
@@ -26,6 +27,7 @@ class LibXsdConan(ConanFile):
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        apply_conandata_patches(self)
 
     def package(self):
         # Licenses
